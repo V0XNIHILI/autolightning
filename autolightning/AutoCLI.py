f61eba40de9235577304e34e684c2a6e06eba075
Douwe den Blanken <douwedb@gmail.com>
2024-08-31 13:09:32 +0200

    Move new AutoCLI code from cli.py to AutoCLI.py

--- autolightning/AutoCLI.py
+++ autolightning/AutoCLI.py
@@ -1,46 +1,193 @@
-from typing import Type, Optional, Dict, Union, Callable, Any
+import sys
+import os
+import tempfile
+import yaml
+from typing import Dict, Any, Union, List, Tuple
 
-from autolightning import AutoModule, AutoDataModule
+import inspect
+from functools import wraps
 
-from lightning.pytorch.cli import LightningCLI, SaveConfigCallback, ArgsType
-from pytorch_lightning import Trainer
+from jsonargparse import ActionConfigFile
+
+import lightning as L
+from lightning.pytorch.cli import LightningCLI, LightningArgumentParser
+import lightning.pytorch as pl
+
+from torch_mate.utils import disable_torch_debug_apis, configure_cuda
+
+
+def create_factory(cls, pre_applied_first_arg, return_annotation):
+    # Get the signature of the __init__ method, excluding 'self'
+    init_signature = inspect.signature(cls.__init__)
+    parameters = list(init_signature.parameters.values())[2:]  # Exclude 'self' and 'cfg'
+    
+    # Create a new signature for the factory function
+    new_signature = inspect.Signature(parameters, return_annotation=return_annotation)
+    
+    # Create the factory function
+    @wraps(cls.__init__)
+    def factory(*args, **kwargs):
+        # Create an instance of the class with the pre-applied first argument
+        return cls(pre_applied_first_arg, *args, **kwargs)
+    
+    # Apply the new signature to the factory function
+    factory.__signature__ = new_signature
+    
+    return factory
+
+
+def pre_cli(module, cfg: Dict):
+    # Get the method resolution order (MRO) of the class
+    mro = inspect.getmro(module)
+
+    if L.LightningDataModule in mro:
+        return_annotation = L.LightningDataModule
+    else:
+        return_annotation = L.LightningModule
+
+    return create_factory(module, cfg, return_annotation)
+
+
+def write_to_temp_file(content):
+    # Create a temporary file
+    temp_file = tempfile.NamedTemporaryFile(delete=False, mode='w', encoding='utf-8', suffix='.yaml')
+    
+    try:
+        # Write content to the temporary file
+        temp_file.write(content)
+        
+        # Return the path of the temporary file
+        return temp_file.name
+    finally:
+        # Close the file
+        temp_file.close()
+
+
+def replace_tuples_with_lists(obj: Union[Dict, List, Tuple]):
+    """Recursively replace all tuples in a nested dictionary or list with lists."""
+    if isinstance(obj, dict):
+        return {k: replace_tuples_with_lists(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [replace_tuples_with_lists(elem) for elem in obj]
+    elif isinstance(obj, tuple):
+        return list(obj)
+    else:
+        return obj
+
+
+class ActionConfigFilePython(ActionConfigFile):
+    """ActionConfigFile with support for configurations stored in Python files with variable name `self.config_var_name` (default: 'cfg')."""
+
+    config_var_name = 'cfg'
+
+    def __call__(self, parser, cfg, values, option_string=None):
+        SEP = ".py"
+
+        if SEP in values:
+            path = values
+
+            # Extract the directory and module name
+            module_dir = os.path.dirname(path)
+            module_name = os.path.splitext(os.path.basename(path))[0]
+
+            # Add the directory to the system path
+            sys.path.append(module_dir)
+
+            # Import the module
+            module = __import__(module_name)
+
+            # Access the variable
+            variable = getattr(module, self.config_var_name)
+            dict_variable = dict(variable)
+
+            yaml_contents = {}
+
+            if "training" in dict_variable:
+                yaml_contents["trainer"] = dict_variable["training"]
+
+            if "learner" in dict_variable:
+                yaml_contents["model"] = {
+                    "class_path": dict_variable["learner"]["name"],
+                    "init_args": {
+                        "cfg": dict_variable
+                    }
+                }
+
+            if "dataset" in dict_variable:
+                yaml_contents["data"] = {
+                    "class_path": dict_variable["dataset"]["name"],
+                    "init_args": {
+                        "cfg": dict_variable
+                    }
+                }
+
+            if "seed" in dict_variable:
+                yaml_contents["seed_everything"] = dict_variable["seed"]
+
+            # Loop through all nested keys and replace all tuples with lists
+            # as jsonargparse's implementation of YAML loading does not support tuples
+            # (see line _loaders_dumpers.py#L19 in jsonargparse project where they
+            # use a SafeLoader instead of a FullLoader which supports tuples)
+            yaml_contents = replace_tuples_with_lists(yaml_contents)
+
+            # Write the contents to a temporary file
+            temp_file_path = write_to_temp_file(yaml.dump(yaml_contents))
+
+            values = temp_file_path
+
+        super().__call__(parser, cfg, values, option_string)
 
 
 class AutoCLI(LightningCLI):
     def __init__(
         self,
-        save_config_callback: Optional[Type[SaveConfigCallback]] = SaveConfigCallback,
-        save_config_kwargs: Optional[Dict[str, Any]] = None,
-        trainer_class: Union[Type[Trainer], Callable[..., Trainer]] = Trainer,
-        trainer_defaults: Optional[Dict[str, Any]] = None,
-        seed_everything_default: Union[bool, int] = True,
-        parser_kwargs: Optional[Union[Dict[str, Any], Dict[str, Dict[str, Any]]]] = None,
-        args: ArgsType = None,
-        run: bool = True,
-        auto_configure_optimizers: bool = True
+        *args,
+        **kwargs
     ):
-        """Small wrapper class around LightningCLI to make it easier to use with AutoModule and AutoDataModule.
-        
-        See the original LightningCLI [docs](https://lightning.ai/docs/pytorch/stable/api/lightning.pytorch.cli.LightningCLI.html) for more information on the possible arguments.
-        """
+        super().__init__(*args, **kwargs)
 
-        full_parser_kwargs = {"parser_mode": "omegaconf"}
+    def init_parser(self, **kwargs: Any) -> LightningArgumentParser:
+        """Method that instantiates the argument parser."""
+        kwargs.setdefault("dump_header", [f"lightning.pytorch=={pl.__version__}"])
+        parser = LightningArgumentParser(**kwargs)
+        parser.add_argument(
+            "-c", "--config", action=ActionConfigFilePython, help="Path to a configuration file in json or yaml format."
+        )
 
-        if parser_kwargs is not None:
-            full_parser_kwargs.update(parser_kwargs)
+        return parser
+    
+    def add_arguments_to_parser(self, parser: LightningArgumentParser) -> None:
+        # Add arguments for disable_torch_debug_apis
+        parser.add_argument("--torch.autograd.set_detect_anomaly", type=bool, default=False, help="Whether to detect anomalies.")
+        parser.add_argument("--torch.autograd.profiler.profile", type=bool, default=True, help="Whether to profile.")
+        parser.add_argument("--torch.autograd.profiler.emit_nvtx", type=bool, default=True, help="Whether to emit nvtx.")
 
-        super().__init__(
-            AutoModule,
-            AutoDataModule,
-            subclass_mode_model=True,
-            subclass_mode_data=True,
-            parser_kwargs=full_parser_kwargs,
-            save_config_callback=save_config_callback,
-            save_config_kwargs=save_config_kwargs,
-            trainer_class=trainer_class,
-            trainer_defaults=trainer_defaults,
-            seed_everything_default=seed_everything_default,
-            args=args,
-            run=run,
-            auto_configure_optimizers=auto_configure_optimizers
+        # Add arguments for configure_cuda
+        parser.add_argument("--torch.set_float32_matmul_precision", type=str, default='highest', help="Precision of float32 matmul (highest, high, medium).")
+        parser.add_argument("--torch.backends.cuda.matmul.allow_tf32", type=bool, default=False, help="Allow TF32 matmul.")
+        parser.add_argument("--torch.backends.cudnn.allow_tf32", type=bool, default=True, help="Allow TF32 cuDNN operations.")
+        parser.add_argument("--torch.backends.cudnn.benchmark", type=bool, default=False, help="Use cuDNN benchmark mode.")
+    
+    def before_instantiate_classes(self):
+        # Get the configuration
+        cfg = self.config[self.config.subcommand]
+
+        torch_cfg = cfg["torch"]
+        torch_autograd_cfg = torch_cfg["autograd"]
+        torch_backends_cfg = torch_cfg["backends"]
+        torch_backends_cudnn_cfg = torch_backends_cfg["cudnn"]
+
+        # Disable PyTorch debug APIs
+        disable_torch_debug_apis(
+            torch_autograd_cfg["set_detect_anomaly"],
+            torch_autograd_cfg["profiler"]["profile"],
+            torch_autograd_cfg["profiler"]["emit_nvtx"]
+        )
+
+        # Configure CUDA
+        configure_cuda(
+            torch_cfg["set_float32_matmul_precision"],
+            torch_backends_cfg["cuda"]["matmul"]["allow_tf32"],
+            torch_backends_cudnn_cfg["allow_tf32"],
+            torch_backends_cudnn_cfg["benchmark"]
         )
