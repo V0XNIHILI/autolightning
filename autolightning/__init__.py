f33c126e508dcd022146ba0eec5936c919fc02c2
Douwe den Blanken <douwedb@gmail.com>
2024-10-16 14:23:46 +0200

    Remove config.py (old)

--- autolightning/__init__.py
+++ autolightning/__init__.py
@@ -1,15 +1,9 @@
 from .AutoModule import AutoModule
 from .AutoDataModule import AutoDataModule
 from .AutoCLI import AutoCLI
-from .config import config_trainer, config_data, config_model, config_model_data, config_all
 
 __all__ = [
     "AutoModule",
     "AutoDataModule",
-    "AutoCLI",
-    "config_trainer",
-    "config_data",
-    "config_model",
-    "config_model_data",
-    "config_all"
+    "AutoCLI"
 ]
