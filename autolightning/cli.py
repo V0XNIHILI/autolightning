7c4f7c98c3d4599c83472104610c786a81301c16
Douwe den Blanken <douwedb@gmail.com>
2024-10-17 09:47:45 +0200

    Remove default AutoDataModule from cli and use absolute imports to be able to run the cli standalone for debugging purposes

--- autolightning/cli.py
+++ autolightning/cli.py
@@ -1,13 +1,11 @@
-from . import AutoModule, AutoDataModule, AutoCLI
-from .auto_cli import LoggerSaveConfigCallback
+from autolightning import AutoModule, AutoCLI
+from autolightning.auto_cli import LoggerSaveConfigCallback
 
 
 def main():
     AutoCLI(
         AutoModule,
-        AutoDataModule,
         subclass_mode_model=True,
-        subclass_mode_data=True,
         save_config_kwargs={"overwrite": True},
         parser_kwargs={"parser_mode": "omegaconf"},
         save_config_callback=LoggerSaveConfigCallback
