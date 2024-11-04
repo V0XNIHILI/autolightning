6085ddb8838da2036b1388bdc4daecb4edb71ad8
Douwe den Blanken <douwedb@gmail.com>
2024-11-04 12:05:59 +0100

    Add support for shuffle_labels args to FewShotMixin

--- autolightning/dm/few_shot.py
+++ autolightning/dm/few_shot.py
@@ -5,7 +5,16 @@ from ..types import Unpack, AutoDataModuleKwargs
 
 
 class FewShotMixin:
-    def __init__(self, ways: int = 5, shots: int = 1, train_ways: int = -1, query_shots: int = -1, query_ways: int = -1, train_query_shots: int = -1, keep_original_labels: bool = False, **kwargs: Unpack[AutoDataModuleKwargs]):
+    def __init__(self,
+                 ways: int = 5,
+                 shots: int = 1,
+                 train_ways: int = -1,
+                 query_shots: int = -1,
+                 query_ways: int = -1,
+                 train_query_shots: int = -1,
+                 keep_original_labels: bool = False,
+                 shuffle_labels: bool = False,
+                 **kwargs: Unpack[AutoDataModuleKwargs]):
         super().__init__(**kwargs)
         
         self.ways = ways
@@ -15,6 +24,7 @@ class FewShotMixin:
         self.query_ways = query_ways
         self.train_query_shots = train_query_shots
         self.keep_original_labels = keep_original_labels
+        self.shuffle_labels = shuffle_labels
 
     def get_transformed_dataset(self, phase: str):
         dataset = super().get_transformed_dataset(phase)
@@ -29,7 +39,7 @@ class FewShotMixin:
             if self.train_query_shots != -1:
                 query_shots = self.train_query_shots
 
-        return FS(dataset, n_way=ways, k_shot=self.shots, query_shots=query_shots, query_ways=self.query_ways, keep_original_labels=self.keep_original_labels)
+        return FS(dataset, n_way=ways, k_shot=self.shots, query_shots=query_shots, query_ways=self.query_ways, keep_original_labels=self.keep_original_labels, shuffle_labels=self.shuffle_labels)
 
 
 class FewShot(FewShotMixin, AutoDataModule):
