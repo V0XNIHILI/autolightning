89cc80ad9870cc8c6fa04be402dbddaa4e10e455
Douwe den Blanken <douwedb@gmail.com>
2024-11-08 12:08:39 +0100

    Make torchvision datasets work again

--- autolightning/datasets.py
+++ autolightning/datasets.py
@@ -1,32 +1,23 @@
 from . import AutoDataModule
 from .types import AutoDataModuleKwargsNoDatsetPrepareSplit, Unpack
 
-
-def _get_auto_data_module_kwargs(dataset: str, root: str, download: bool, val_percentage: float):
-    kwargs = {}
-
-    kwargs["dataset"] = {
-        "class": f"torchvision.datasets.{dataset}" if "." not in dataset else dataset,
-        "args": dict(defaults=dict(root=root,
+class RootDownloadTrain(AutoDataModule):
+    def __init__(self, name: str, root: str, download: bool = False, val_percentage: float = 0.1, **kwargs: Unpack[AutoDataModuleKwargsNoDatsetPrepareSplit]):
+        super().__init__(
+            dataset={
+                "class": f"torchvision.datasets.{name}" if "." not in name else name,
+                "args": dict(defaults=dict(root=root,
                                     train=True,
                                     download=download),
                         test=dict(train=False)),
-    }
-
-    kwargs["random_split"] = {
-        "source": "defaults",
-        "dest": dict(train=1-val_percentage,
-                     val=val_percentage),
-    }
-
-    kwargs["requires_prepare"] = download
-
-    return kwargs
-
-
-class RootDownloadTrain(AutoDataModule):
-    def __init__(self, name: str, root: str, download: bool = False, val_percentage: float = 0.1, **kwargs: Unpack[AutoDataModuleKwargsNoDatsetPrepareSplit]):
-        super().__init__(**_get_auto_data_module_kwargs(name, root, download, val_percentage), **kwargs)
+            },
+            random_split={
+                "source": "defaults",
+                "dest": dict(train=1-val_percentage,
+                             val=val_percentage),
+            },
+            requires_prepare=download,
+            **kwargs)
 
 
 class MNIST(RootDownloadTrain):
