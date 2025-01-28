e139231605c7f22af880680f6486f4c678922dd2
Douwe den Blanken <douwedb@gmail.com>
2025-01-28 14:39:13 +0100

    Add whiteline to classifier.py

--- autolightning/lm/classifier.py
+++ autolightning/lm/classifier.py
@@ -8,6 +8,7 @@ from torch_mate.utils import calc_accuracy
 from .supervised import Supervised
 from ..types import AutoModuleKwargsNoCriterion, Unpack
 
+
 class ClassifierMixin:
     def __init__(self, top_k: int = 1, criterion: Optional[nn.Module] = None, **kwargs: Unpack[AutoModuleKwargsNoCriterion]):
         if criterion == None:
