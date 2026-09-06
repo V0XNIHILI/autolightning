12ea7906bcc13771907994103a2d572fd0d2a4f5
Douwe den Blanken <douwedb@gmail.com>
2026-09-06 15:51:45 +0200

    Create basic callback to save initial checkpoint of training

--- autolightning/callbacks.py
+++ autolightning/callbacks.py
@@ -1,8 +1,8 @@
 import os
-from pytorch_lightning.callbacks import Callback
+from lightning.pytorch.callbacks import Callback
 
 class SaveInitialCheckpoint(Callback):
-    def on_train_start(self, trainer, pl_module):
+    def on_fit_start(self, trainer, pl_module):
         path = os.path.join(trainer.default_root_dir, "checkpoints", "initial.ckpt")
         os.makedirs(os.path.dirname(path), exist_ok=True)
         trainer.save_checkpoint(path)
